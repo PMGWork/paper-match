99750fb9dd488258d41fd948d9d8e18d12770c3b
PMGWork <48606231+PMGWork@users.noreply.github.com>
2025-06-17 00:41:08 +0900

    Initial Commit

---
+++ paperMatch/paperMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  paperMatchApp.swift
+//  paperMatch
+//
+//  Created by chiba yuto on 2025/06/17.
+//
+
+import SwiftUI
+
+@main
+struct paperMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
